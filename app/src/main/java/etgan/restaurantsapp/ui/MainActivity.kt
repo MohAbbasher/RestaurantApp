2d03683b7048b4ec32bab9a17451c59cf26ca001
MohAbbasher <moh.abbasher@gmail.com>
2020-10-27 13:25:28 +0200

    first commit - create MVVM project structure

---
+++ app/src/main/java/etgan/restaurantsapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package etgan.restaurantsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import etgan.restaurantsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
